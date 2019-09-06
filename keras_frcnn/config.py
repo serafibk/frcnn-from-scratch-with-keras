a1036f1a046a882722391a911f1109d26fa3663b
kentaroy47 <kentaroy@stanford.edu>
2019-09-06 14:09:23 +0000

    update config

--- keras_frcnn/config.py
+++ keras_frcnn/config.py
@@ -18,7 +18,7 @@ class Config:
 		self.anchor_box_scales = [128, 256, 512]
 
 		# anchor box ratios
-		self.anchor_box_ratios = [[1, 1], [1./math.sqrt(2), 2./math.sqrt(2)], [2./math.sqrt(2), 1./math.sqrt(2)]]
+		self.anchor_box_ratios = [[1, 1], [1, 2], [2, 1]]
 
 		# size to resize the smallest side of the image
 		self.im_size = 600
@@ -28,7 +28,7 @@ class Config:
 		self.img_scaling_factor = 1.0
 
 		# number of ROIs at once
-		self.num_rois = 4
+		self.num_rois = 300
 
 		# stride at the RPN (this depends on the network configuration)
 		self.rpn_stride = 16
