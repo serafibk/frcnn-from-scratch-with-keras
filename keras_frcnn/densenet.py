bf822ab43f442cff0caf69623005badef2a2f809
Kentaro Yoshioka <meathouse47@gmail.com>
2019-10-08 15:17:29 +0900

    Update densenet.py
    add densenet from keras.model

--- keras_frcnn/densenet.py
+++ keras_frcnn/densenet.py
@@ -1,202 +1,215 @@
-# -*- coding: utf-8 -*-
-'''ResNet50 model for Keras.
-# Reference:
-- [Deep Residual Learning for Image Recognition](https://arxiv.org/abs/1512.03385)
-Adapted from code contributed by BigMoyan.
-'''
-from __future__ import print_function
+"""DenseNet models for Keras.
+# Reference paper
+- [Densely Connected Convolutional Networks]
+  (https://arxiv.org/abs/1608.06993) (CVPR 2017 Best Paper Award)
+# Reference implementation
+- [Torch DenseNets]
+  (https://github.com/liuzhuang13/DenseNet/blob/master/models/densenet.lua)
+- [TensorNets]
+  (https://github.com/taehoonlee/tensornets/blob/master/tensornets/densenets.py)
+"""
 from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
-from keras.layers import Input, Add, Dense, Activation, Flatten, Convolution2D, MaxPooling2D, ZeroPadding2D, \
-    AveragePooling2D, TimeDistributed, BatchNormalization
+import os
 
-from keras import backend as K
-import os 
-from keras_frcnn.RoiPoolingConv import RoiPoolingConv
-from keras_frcnn.FixedBatchNormalization import FixedBatchNormalization
+from . import get_submodules_from_kwargs
+from . import imagenet_utils
+from .imagenet_utils import decode_predictions
+from .imagenet_utils import _obtain_input_shape
 
 
-def get_weight_path():
-    if K.image_dim_ordering() == 'th':
-        return 'resnet50_weights_th_dim_ordering_th_kernels_notop.h5'
-    else:
-        return os.path.join("pretrain", "resnet50_weights_tf_dim_ordering_tf_kernels.h5")
+BASE_WEIGTHS_PATH = (
+    'https://github.com/keras-team/keras-applications/'
+    'releases/download/densenet/')
+DENSENET121_WEIGHT_PATH = (
+    BASE_WEIGTHS_PATH +
+    'densenet121_weights_tf_dim_ordering_tf_kernels.h5')
+DENSENET121_WEIGHT_PATH_NO_TOP = (
+    BASE_WEIGTHS_PATH +
+    'densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5')
+DENSENET169_WEIGHT_PATH = (
+    BASE_WEIGTHS_PATH +
+    'densenet169_weights_tf_dim_ordering_tf_kernels.h5')
+DENSENET169_WEIGHT_PATH_NO_TOP = (
+    BASE_WEIGTHS_PATH +
+    'densenet169_weights_tf_dim_ordering_tf_kernels_notop.h5')
+DENSENET201_WEIGHT_PATH = (
+    BASE_WEIGTHS_PATH +
+    'densenet201_weights_tf_dim_ordering_tf_kernels.h5')
+DENSENET201_WEIGHT_PATH_NO_TOP = (
+    BASE_WEIGTHS_PATH +
+    'densenet201_weights_tf_dim_ordering_tf_kernels_notop.h5')
 
-def get_img_output_length(width, height):
-    def get_output_length(input_length):
-        # zero_pad
-        input_length += 6
-        # apply 4 strided convolutions
-        filter_sizes = [7, 3, 1, 1]
-        stride = 2
-        for filter_size in filter_sizes:
-            input_length = (input_length - filter_size + stride) // stride
-        return input_length
+backend = None
+layers = None
+models = None
+keras_utils = None
 
-    return get_output_length(width), get_output_length(height) 
 
-def identity_block(input_tensor, kernel_size, filters, stage, block, trainable=True):
-
-    nb_filter1, nb_filter2, nb_filter3 = filters
-    
-    if K.image_dim_ordering() == 'tf':
-        bn_axis = 3
-    else:
-        bn_axis = 1
-
-    conv_name_base = 'res' + str(stage) + block + '_branch'
-    bn_name_base = 'bn' + str(stage) + block + '_branch'
-
-    x = Convolution2D(nb_filter1, (1, 1), name=conv_name_base + '2a', trainable=trainable)(input_tensor)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
-    x = Activation('relu')(x)
-
-    x = Convolution2D(nb_filter2, (kernel_size, kernel_size), padding='same', name=conv_name_base + '2b', trainable=trainable)(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
-    x = Activation('relu')(x)
-
-    x = Convolution2D(nb_filter3, (1, 1), name=conv_name_base + '2c', trainable=trainable)(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
-
-    x = Add()([x, input_tensor])
-    x = Activation('relu')(x)
+def dense_block(x, blocks, name):
+    """A dense block.
+    # Arguments
+        x: input tensor.
+        blocks: integer, the number of building blocks.
+        name: string, block label.
+    # Returns
+        output tensor for the block.
+    """
+    for i in range(blocks):
+        x = conv_block(x, 32, name=name + '_block' + str(i + 1))
     return x
 
 
-def identity_block_td(input_tensor, kernel_size, filters, stage, block, trainable=True):
-
-    # identity block time distributed
-
-    nb_filter1, nb_filter2, nb_filter3 = filters
-    if K.image_dim_ordering() == 'tf':
-        bn_axis = 3
-    else:
-        bn_axis = 1
-
-    conv_name_base = 'res' + str(stage) + block + '_branch'
-    bn_name_base = 'bn' + str(stage) + block + '_branch'
-
-    x = TimeDistributed(Convolution2D(nb_filter1, (1, 1), trainable=trainable, kernel_initializer='normal'), name=conv_name_base + '2a')(input_tensor)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2a')(x)
-    x = Activation('relu')(x)
-
-    x = TimeDistributed(Convolution2D(nb_filter2, (kernel_size, kernel_size), trainable=trainable, kernel_initializer='normal',padding='same'), name=conv_name_base + '2b')(x)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2b')(x)
-    x = Activation('relu')(x)
-
-    x = TimeDistributed(Convolution2D(nb_filter3, (1, 1), trainable=trainable, kernel_initializer='normal'), name=conv_name_base + '2c')(x)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2c')(x)
-
-    x = Add()([x, input_tensor])
-    x = Activation('relu')(x)
-
-    return x
-
-def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2), trainable=True):
-
-    nb_filter1, nb_filter2, nb_filter3 = filters
-    if K.image_dim_ordering() == 'tf':
-        bn_axis = 3
-    else:
-        bn_axis = 1
-
-    conv_name_base = 'res' + str(stage) + block + '_branch'
-    bn_name_base = 'bn' + str(stage) + block + '_branch'
-
-    x = Convolution2D(nb_filter1, (1, 1), strides=strides, name=conv_name_base + '2a', trainable=trainable)(input_tensor)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
-    x = Activation('relu')(x)
-
-    x = Convolution2D(nb_filter2, (kernel_size, kernel_size), padding='same', name=conv_name_base + '2b', trainable=trainable)(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
-    x = Activation('relu')(x)
-
-    x = Convolution2D(nb_filter3, (1, 1), name=conv_name_base + '2c', trainable=trainable)(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
-
-    shortcut = Convolution2D(nb_filter3, (1, 1), strides=strides, name=conv_name_base + '1', trainable=trainable)(input_tensor)
-    shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
-
-    x = Add()([x, shortcut])
-    x = Activation('relu')(x)
+def transition_block(x, reduction, name):
+    """A transition block.
+    # Arguments
+        x: input tensor.
+        reduction: float, compression rate at transition layers.
+        name: string, block label.
+    # Returns
+        output tensor for the block.
+    """
+    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                  name=name + '_bn')(x)
+    x = layers.Activation('relu', name=name + '_relu')(x)
+    x = layers.Conv2D(int(backend.int_shape(x)[bn_axis] * reduction), 1,
+                      use_bias=False,
+                      name=name + '_conv')(x)
+    x = layers.AveragePooling2D(2, strides=2, name=name + '_pool')(x)
     return x
 
 
-def conv_block_td(input_tensor, kernel_size, filters, stage, block, input_shape, strides=(2, 2), trainable=True):
-
-    # conv block time distributed
-
-    nb_filter1, nb_filter2, nb_filter3 = filters
-    if K.image_dim_ordering() == 'tf':
-        bn_axis = 3
-    else:
-        bn_axis = 1
-
-    conv_name_base = 'res' + str(stage) + block + '_branch'
-    bn_name_base = 'bn' + str(stage) + block + '_branch'
-
-    x = TimeDistributed(Convolution2D(nb_filter1, (1, 1), strides=strides, trainable=trainable, kernel_initializer='normal'), input_shape=input_shape, name=conv_name_base + '2a')(input_tensor)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2a')(x)
-    x = Activation('relu')(x)
-
-    x = TimeDistributed(Convolution2D(nb_filter2, (kernel_size, kernel_size), padding='same', trainable=trainable, kernel_initializer='normal'), name=conv_name_base + '2b')(x)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2b')(x)
-    x = Activation('relu')(x)
-
-    x = TimeDistributed(Convolution2D(nb_filter3, (1, 1), kernel_initializer='normal'), name=conv_name_base + '2c', trainable=trainable)(x)
-    x = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '2c')(x)
-
-    shortcut = TimeDistributed(Convolution2D(nb_filter3, (1, 1), strides=strides, trainable=trainable, kernel_initializer='normal'), name=conv_name_base + '1')(input_tensor)
-    shortcut = TimeDistributed(BatchNormalization(axis=bn_axis), name=bn_name_base + '1')(shortcut)
-
-    x = Add()([x, shortcut])
-    x = Activation('relu')(x)
+def conv_block(x, growth_rate, name):
+    """A building block for a dense block.
+    # Arguments
+        x: input tensor.
+        growth_rate: float, growth rate at dense layers.
+        name: string, block label.
+    # Returns
+        Output tensor for the block.
+    """
+    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+    x1 = layers.BatchNormalization(axis=bn_axis,
+                                   epsilon=1.001e-5,
+                                   name=name + '_0_bn')(x)
+    x1 = layers.Activation('relu', name=name + '_0_relu')(x1)
+    x1 = layers.Conv2D(4 * growth_rate, 1,
+                       use_bias=False,
+                       name=name + '_1_conv')(x1)
+    x1 = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                   name=name + '_1_bn')(x1)
+    x1 = layers.Activation('relu', name=name + '_1_relu')(x1)
+    x1 = layers.Conv2D(growth_rate, 3,
+                       padding='same',
+                       use_bias=False,
+                       name=name + '_2_conv')(x1)
+    x = layers.Concatenate(axis=bn_axis, name=name + '_concat')([x, x1])
     return x
 
-def nn_base(input_tensor=None, trainable=False):
+def nn_base(blocks,
+             include_top=False,
+             weights='imagenet',
+             input_tensor=None,
+             input_shape=None,
+             pooling=None,
+             classes=1000,
+             **kwargs):
+    """Instantiates the DenseNet architecture.
+    Optionally loads weights pre-trained on ImageNet.
+    Note that the data format convention used by the model is
+    the one specified in your Keras config at `~/.keras/keras.json`.
+    # Arguments
+        blocks: numbers of building blocks for the four dense layers.
+        include_top: whether to include the fully-connected
+            layer at the top of the network.
+        weights: one of `None` (random initialization),
+              'imagenet' (pre-training on ImageNet),
+              or the path to the weights file to be loaded.
+        input_tensor: optional Keras tensor
+            (i.e. output of `layers.Input()`)
+            to use as image input for the model.
+        input_shape: optional shape tuple, only to be specified
+            if `include_top` is False (otherwise the input shape
+            has to be `(224, 224, 3)` (with `'channels_last'` data format)
+            or `(3, 224, 224)` (with `'channels_first'` data format).
+            It should have exactly 3 inputs channels,
+            and width and height should be no smaller than 32.
+            E.g. `(200, 200, 3)` would be one valid value.
+        pooling: optional pooling mode for feature extraction
+            when `include_top` is `False`.
+            - `None` means that the output of the model will be
+                the 4D tensor output of the
+                last convolutional block.
+            - `avg` means that global average pooling
+                will be applied to the output of the
+                last convolutional block, and thus
+                the output of the model will be a 2D tensor.
+            - `max` means that global max pooling will
+                be applied.
+        classes: optional number of classes to classify images
+            into, only to be specified if `include_top` is True, and
+            if no `weights` argument is specified.
+    # Returns
+        A Keras model instance.
+    # Raises
+        ValueError: in case of invalid argument for `weights`,
+            or invalid input shape.
+    """
+
+   global backend, layers, models, keras_utils
+    backend, layers, models, keras_utils = get_submodules_from_kwargs(kwargs)
+
+    if not (weights in {'imagenet', None} or os.path.exists(weights)):
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization), `imagenet` '
+                         '(pre-training on ImageNet), '
+                         'or the path to the weights file to be loaded.')
+
+    if weights == 'imagenet' and include_top and classes != 1000:
+        raise ValueError('If using `weights` as `"imagenet"` with `include_top`'
+                         ' as true, `classes` should be 1000')
 
     # Determine proper input shape
-    if K.image_dim_ordering() == 'th':
-        input_shape = (3, None, None)
-    else:
-        input_shape = (None, None, 3)
+    input_shape = _obtain_input_shape(input_shape,
+                                      default_size=224,
+                                      min_size=32,
+                                      data_format=backend.image_data_format(),
+                                      require_flatten=include_top,
+                                      weights=weights)
 
     if input_tensor is None:
-        img_input = Input(shape=input_shape)
+        img_input = layers.Input(shape=input_shape)
     else:
-        if not K.is_keras_tensor(input_tensor):
-            img_input = Input(tensor=input_tensor, shape=input_shape)
+        if not backend.is_keras_tensor(input_tensor):
+            img_input = layers.Input(tensor=input_tensor, shape=input_shape)
         else:
             img_input = input_tensor
 
-    if K.image_dim_ordering() == 'tf':
-        bn_axis = 3
-    else:
-        bn_axis = 1
+    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
 
-    x = ZeroPadding2D((3, 3))(img_input)
+    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(img_input)
+    x = layers.Conv2D(64, 7, strides=2, use_bias=False, name='conv1/conv')(x)
+    x = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name='conv1/bn')(x)
+    x = layers.Activation('relu', name='conv1/relu')(x)
+    x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
+    x = layers.MaxPooling2D(3, strides=2, name='pool1')(x)
 
-    x = Convolution2D(64, (7, 7), strides=(2, 2), name='conv1', trainable = trainable)(x)
-    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
-    x = Activation('relu')(x)
-    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+    x = dense_block(x, blocks[0], name='conv2')
+    x = transition_block(x, 0.5, name='pool2')
+    x = dense_block(x, blocks[1], name='conv3')
+    x = transition_block(x, 0.5, name='pool3')
+    x = dense_block(x, blocks[2], name='conv4')
+    x = transition_block(x, 0.5, name='pool4')
+    x = dense_block(x, blocks[3], name='conv5')
 
-    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1), trainable = trainable)
-    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b', trainable = trainable)
-    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c', trainable = trainable)
-
-    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a', trainable = trainable)
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b', trainable = trainable)
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c', trainable = trainable)
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d', trainable = trainable)
-
-    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a', trainable = trainable)
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b', trainable = trainable)
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c', trainable = trainable)
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d', trainable = trainable)
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e', trainable = trainable)
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f', trainable = trainable)
-
+    x = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name='bn')(x)
+    x = layers.Activation('relu', name='relu')(x)
+    
     return x
 
 
